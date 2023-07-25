c830b8b1b6d815fb90681d7268d56080da22f785
Nicolas <nicolas.parucker@hotmail.com>
2023-07-25 06:27:35 +0200

    added board size constant

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
-CELL_SIZE: int = 9
+CELL_SIZE: int = 7
 WINDOW_HEIGHT: int = 900
 WINDOW_WIDTH: int = 900
 IT_DELAY: float = 0.1  # seconds
+BOARD_SIZE: tuple[int, int] = 700, 700
 
 # -- JSON --
 JSON_FILE_MODE: str = "r"
