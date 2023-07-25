c85632e7cdc8a654d78e7bd279726172ca6a489b
Nicolas <nicolas.parucker@hotmail.com>
2023-07-25 11:07:14 +0200

    added cycle buttons object

--- src/gui/gui_manager.py
+++ src/gui/gui_manager.py
@@ -1,11 +1,14 @@
 import pygame
 
-from src.gui.veriation_title import VariationTitleGui
+from src.gui.verification_title_gui import VariationTitleGui
+from src.gui.cycle_buttons_gui import CycleButtonsGui
 
 
 class GuiManager:
     def __init__(self, board_rect: pygame.rect.Rect) -> None:
         self.variation_title: VariationTitleGui = VariationTitleGui(board_rect)
+        self.cycle_buttons: CycleButtonsGui = CycleButtonsGui(board_rect)
 
     def draw(self) -> None:
         self.variation_title.draw()
+        self.cycle_buttons.draw()
