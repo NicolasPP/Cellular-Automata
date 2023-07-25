0fc74116a0163d48576062182d78e457d6f8d2bc
Nicolas <nicolas.parucker@hotmail.com>
2023-07-25 06:28:08 +0200

    creating board_rect before main loop

--- src/main.py
+++ src/main.py
@@ -7,13 +7,16 @@ from src.automata.variation_manager import VariationManager
 from src.config import CELL_SIZE
 from src.config import WINDOW_HEIGHT
 from src.config import WINDOW_WIDTH
+from src.config import BOARD_SIZE
 
 done: bool = False
-automata_manager: CellularAutomata = CellularAutomata(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
+pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+board_rect: pygame.rect.Rect = pygame.rect.Rect(0, 0, *BOARD_SIZE)
+board_rect.center = pygame.display.get_surface().get_rect().center
+automata_manager: CellularAutomata = CellularAutomata(board_rect, CELL_SIZE)
 prev_time: float = time.time()
 delta_time: float = 0.0
 
-pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 while not done:
 
@@ -27,14 +30,14 @@ while not done:
             done = True
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            automata_manager.check_collision()
+            automata_manager.board.check_collision()
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 automata_manager.iterate_switch()
 
             if event.key == pygame.K_r:
-                automata_manager.randomise()
+                automata_manager.board.randomise()
 
             if event.key == pygame.K_RIGHT:
                 VariationManager.cycle(1)
