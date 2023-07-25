0e476c282647dffe33d8b7ca1496712d72eca8b4
Nicolas <nicolas.parucker@hotmail.com>
2023-07-25 06:42:11 +0200

    created main loop function and init function which is now called in the if __name__ == __main__

--- src/main.py
+++ src/main.py
@@ -1,54 +1,67 @@
+import dataclasses
 import time
 
 import pygame
 
 from src.automata.cellular_automata import CellularAutomata
 from src.automata.variation_manager import VariationManager
+from src.config import BOARD_SIZE
 from src.config import CELL_SIZE
 from src.config import WINDOW_HEIGHT
 from src.config import WINDOW_WIDTH
-from src.config import BOARD_SIZE
-
-done: bool = False
-pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-board_rect: pygame.rect.Rect = pygame.rect.Rect(0, 0, *BOARD_SIZE)
-board_rect.center = pygame.display.get_surface().get_rect().center
-automata_manager: CellularAutomata = CellularAutomata(board_rect, CELL_SIZE)
-prev_time: float = time.time()
-delta_time: float = 0.0
 
 
-while not done:
+@dataclasses.dataclass
+class AppData:
+    done: bool
+    board_rect: pygame.rect.Rect
+    automata: CellularAutomata
+    prev_time: float
+    delta_time: float
 
-    now: float = time.time()
-    delta_time = now - prev_time
-    prev_time = now
 
-    for event in pygame.event.get():
+def init() -> AppData:
+    pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    VariationManager.load_rules()
+    board_rect: pygame.rect.Rect = pygame.rect.Rect(0, 0, *BOARD_SIZE)
+    board_rect.center = pygame.display.get_surface().get_rect().center
+    return AppData(False, board_rect, CellularAutomata(board_rect, CELL_SIZE), 0.0, 0.0)
 
-        if event.type == pygame.QUIT:
-            done = True
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            automata_manager.board.check_collision()
+def main_loop(app_data: AppData) -> None:
+    while not app_data.done:
+        now: float = time.time()
+        app_data.delta_time = now - app_data.prev_time
+        app_data.prev_time = now
 
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                automata_manager.iterate_switch()
+        for event in pygame.event.get():
 
-            if event.key == pygame.K_r:
-                automata_manager.board.randomise()
+            if event.type == pygame.QUIT:
+                app_data.done = True
 
-            if event.key == pygame.K_RIGHT:
-                VariationManager.cycle(1)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                app_data.automata.board.check_collision()
 
-            if event.key == pygame.K_LEFT:
-                VariationManager.cycle(-1)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    app_data.automata.iterate_switch()
 
-    pygame.display.get_surface().fill("black")
-    automata_manager.draw()
-    automata_manager.iterate(delta_time)
+                if event.key == pygame.K_r:
+                    app_data.automata.board.randomise()
 
-    pygame.display.update()
+                if event.key == pygame.K_RIGHT:
+                    VariationManager.cycle(1)
 
-pygame.quit()
+                if event.key == pygame.K_LEFT:
+                    VariationManager.cycle(-1)
+
+        pygame.display.get_surface().fill("black")
+        app_data.automata.draw()
+        app_data.automata.iterate(app_data.delta_time)
+        pygame.display.update()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main_loop(init())
