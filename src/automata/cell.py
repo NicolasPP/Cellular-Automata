999bf17830528a2eb43a2f53bfa74dc8894cd033
Nicolas <nicolas.parucker@hotmail.com>
2023-07-25 06:54:47 +0200

    Changed states color

--- src/automata/cell.py
+++ src/automata/cell.py
@@ -2,21 +2,20 @@ import enum
 
 import pygame
 
+from src.config import ALIVE_COLOR
+from src.config import DEAD_COLOR
+
 
 class CellState(enum.Enum):
-    ALIVE = "black"
-    DEAD = "white"
+    ALIVE = enum.auto()
+    DEAD = enum.auto()
 
 
 class Cell:
+    state_colors: dict[CellState, tuple[int, int, int]] = {CellState.ALIVE: ALIVE_COLOR, CellState.DEAD: DEAD_COLOR}
 
-    def __init__(self,
-                 x: int,
-                 y: int,
-                 size: int,
-                 neighbours: list[tuple[int, int]],
-                 container: pygame.surface.Surface
-                 ) -> None:
+    def __init__(self, x: int, y: int, size: int, neighbours: list[tuple[int, int]],
+                 container: pygame.surface.Surface) -> None:
         self.rect: pygame.rect.Rect = pygame.rect.Rect(x, y, size, size)
         self.surface: pygame.surface.Surface = pygame.surface.Surface((size, size))
         self.state: CellState = CellState.DEAD
@@ -27,5 +26,5 @@ class Cell:
         self.state = state
 
     def draw(self) -> None:
-        self.surface.fill(self.state.value)
+        self.surface.fill(Cell.state_colors[self.state])
         self.container.blit(self.surface, self.rect)
--- src/config.py
+++ src/config.py
@@ -3,7 +3,10 @@ WINDOW_HEIGHT: int = 900
 WINDOW_WIDTH: int = 900
 IT_DELAY: float = 0.1  # seconds
 BOARD_SIZE: tuple[int, int] = 700, 700
-BACKGROUND: tuple[int, int, int] = 35, 40, 49
+BACKGROUND: tuple[int, int, int] = 27, 32, 39
+ALIVE_COLOR: tuple[int, int, int] = 170, 79, 30
+DEAD_COLOR: tuple[int, int, int] = 35, 40, 49
+
 
 # -- JSON --
 JSON_FILE_MODE: str = "r"
