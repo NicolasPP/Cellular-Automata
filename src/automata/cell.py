5429324d3c0d7d696e2247d823b63831ed86b8fb
Nicolas <nicolas.parucker@hotmail.com>
2023-07-25 06:19:39 +0200

    board class is now rendered into a container which is described the rect passed into automata

--- src/automata/cell.py
+++ src/automata/cell.py
@@ -10,19 +10,22 @@ class CellState(enum.Enum):
 
 class Cell:
 
-    def __init__(self, x: int, y: int, size: int) -> None:
+    def __init__(self,
+                 x: int,
+                 y: int,
+                 size: int,
+                 neighbours: list[tuple[int, int]],
+                 container: pygame.surface.Surface
+                 ) -> None:
         self.rect: pygame.rect.Rect = pygame.rect.Rect(x, y, size, size)
         self.surface: pygame.surface.Surface = pygame.surface.Surface((size, size))
         self.state: CellState = CellState.DEAD
-        self.neighbours: list[tuple[int, int]] = []
+        self.neighbours: list[tuple[int, int]] = neighbours
+        self.container: pygame.surface.Surface = container
 
     def set_state(self, state: CellState) -> None:
         self.state = state
 
-    def set_neighbours(self, neighbours: list[tuple[int, int]]) -> None:
-        assert len(neighbours) > 0, "LifeCell must have more than 0 neighbours"
-        self.neighbours = neighbours
-
     def draw(self) -> None:
         self.surface.fill(self.state.value)
-        pygame.display.get_surface().blit(self.surface, self.rect)
+        self.container.blit(self.surface, self.rect)
--- src/automata/cellular_automata.py
+++ src/automata/cellular_automata.py
@@ -1,8 +1,6 @@
-import random
-import typing
-
 import pygame
 
+from src.automata.board import Board
 from src.automata.cell import Cell
 from src.automata.cell import CellState
 from src.automata.variation_manager import VariationManager
@@ -23,63 +21,18 @@ class Accumulator:
 
 
 class CellularAutomata:
-
-    @staticmethod
-    def create_board(window_width: int, window_height: int, cell_size: int) -> list[list[Cell]]:
-        board: list[list[Cell]] = []
-        columns: int = window_width // cell_size
-        rows: int = window_height // cell_size
-
-        possible_neighbours: list[tuple[int, int]] = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (1, -1),
-                                                      (-1, -1)]
-
-        for row in range(rows):
-            board_row: list[Cell] = []
-            for col in range(columns):
-
-                neighbours: list[tuple[int, int]] = []
-
-                for n_offset in possible_neighbours:
-                    x_offset, y_offset = n_offset
-
-                    if 0 <= (row + y_offset) < rows and 0 <= (col + x_offset) < columns:
-                        neighbours.append((col + x_offset, row + y_offset))
-
-                cell: Cell = Cell(col * cell_size, row * cell_size, cell_size)
-                cell.set_neighbours(neighbours)
-                board_row.append(cell)
-
-            board.append(board_row)
-
-        return board
-
-    def __init__(self, window_width: int, window_height: int, cell_size: int) -> None:
+    def __init__(self, board_rect: pygame.rect.Rect, cell_size: int) -> None:
         VariationManager.load_rules()
-        self.width: int = window_width
-        self.height: int = window_height
         self.cell_size: int = cell_size
-        self.board: list[list[Cell]] = CellularAutomata.create_board(window_width, window_height, cell_size)
+        self.board: Board = Board(board_rect, cell_size)
         self.iterate_board: bool = False
 
-    def board_it(self) -> typing.Generator[Cell, None, None]:
-        for board_rows in self.board:
-            for cell in board_rows:
-                yield cell
-
     def draw(self) -> None:
-        for cell in self.board_it():
-            cell.draw()
+        self.board.draw()
+        pygame.display.get_surface().blit(self.board.container, self.board.rect)
 
     def iterate_switch(self) -> None:
         self.iterate_board = not self.iterate_board
-
-    def check_collision(self) -> None:
-        x, y = pygame.mouse.get_pos()
-        col: int = y // self.cell_size
-        row: int = x // self.cell_size
-        if col >= self.height // self.cell_size or row >= self.width // self.cell_size:
-            return
-        self.board[col][row].set_state(CellState.ALIVE)
 
     def iterate(self, delta_time: float, acc: Accumulator = Accumulator(IT_DELAY)) -> None:
         if not self.iterate_board: return
@@ -93,7 +46,3 @@ class CellularAutomata:
 
             elif cell.state is CellState.DEAD:
                 cell.set_state(CellState.ALIVE)
-
-    def randomise(self) -> None:
-        state: dict[int, CellState] = {0: CellState.ALIVE, 1: CellState.DEAD}
-        for cell in self.board_it(): cell.set_state(state[random.randint(0, 1)])
